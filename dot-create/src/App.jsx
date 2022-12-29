ddbd6007a49c2154c06e4c6606212a2c382c6207
Joabe Ferri <joabeferri10@gmail.com>
2022-12-28 22:31:20 -0300

    App complete
    App completo, com função de desfazer e refazer os Dots

--- dot-create/src/App.jsx
+++ dot-create/src/App.jsx
@@ -3,30 +3,81 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([]);
+  const [undid, setUndid] = useState([]);
+
+  const handleClick = (event) => {
+
+    const newDot = {
+      clientX: event.clientX,
+      clientY: event.clientY
+    };
+
+    // console.log(newDot);
+    setList((prev) => {
+      return [...prev, newDot];
+    });
+    setUndid([]);
+  }
+
+  const notAction = (event) => {
+    event.stopPropagation();
+  }
+
+  const handleUndo = (event) => {
+    event.stopPropagation();
+    // console.log('Undo');
+
+    if(list.length === 0) {
+      alert('Não há mais dots para desfazer')
+      return
+    }
+
+    const lastItem = list[list.length - 1];
+    setUndid((prev) => [...prev, lastItem])
+
+    setList((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    })
+  }
+
+  const handleRedo = (event) => {
+    event.stopPropagation();
+    // console.log('Redo')
+
+    if(undid.length === 0) {
+      alert('Não há mais dots para refazer')
+      return
+    }
+
+    const recoveriDot = undid[undid.length - 1];
+
+    setUndid((prev) => {
+      const newArr = [...prev].slice(0, -1);
+      return newArr;
+    });
+
+    setList((prev) => {
+      return [...prev, recoveriDot];
+    });
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id='clickable' onClick={handleClick}>
+
+      <div className='buttons-space'>
+        <button className='button-margin' onClick={handleUndo}>Desfazer</button>
+        <button onClick={handleRedo}>Retornar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        {list.map(item =>  
+        (<span 
+          key={item.clientX}
+          onClick={notAction} 
+          className='dot' 
+          style={{top: item.clientY - 8,left: item.clientX - 8}}/>)
+        )}
     </div>
   )
 }
